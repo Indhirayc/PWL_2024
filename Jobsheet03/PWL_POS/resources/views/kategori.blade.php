d146d1454ca5f5c856fb66f3648ea8375176bd39
Indhirayc <indhirayuantikac1176@gmail.com>
2026-03-02 09:25:05 +0700

    Jobsheet03_Praktikum05

---
+++ Jobsheet03/PWL_POS/resources/views/kategori.blade.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Data Kategori Barang</title>
+</head>
+<body>
+    <h1>Data Kategori Barang</h1>
+    <table border="1" cellpadding="2" cellspacing="0">
+        <thead>
+            <tr>
+                <th>ID</th>
+                <th>Kode Kategori</th>
+                <th>Nama Kategori</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach($data as $kategori)
+            <tr>
+                <td>{{ $kategori->kategori_id }}</td>  
+                <td>{{ $kategori->kategori_kode }}</td>
+                <td>{{ $kategori->kategori_nama }}</td>
+            </tr>
+            @endforeach
+        </tbody>
+    </table>    
+</body>
+</html>
