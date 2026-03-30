2610b8ace596c02270a016107543f4cba8aa6335
Indhirayc <indhirayuantikac1176@gmail.com>
2026-03-30 11:29:03 +0700

    Jobsheet05

--- Jobsheet03/PWL_POS/app/Models/LevelModel.php
+++ Jobsheet03/PWL_POS/app/Models/LevelModel.php
@@ -9,7 +9,7 @@ class LevelModel extends Model
 {
     use HasFactory;
 
-    protected $table = 'm_level'; // ganti dengan nama tabel level
+    protected $table = 'm_level'; 
     protected $primaryKey = 'level_id';
-    protected $fillable = ['level_name']; // sesuaikan kolom tabel
+    protected $fillable = ['level_name']; 
 }
