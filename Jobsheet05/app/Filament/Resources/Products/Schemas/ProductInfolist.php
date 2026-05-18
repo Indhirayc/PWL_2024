b73d5c426b13572e43ac8426f655b61f8eabb4e6
Indhirayc <indhirayuantikac1176@gmail.com>
2026-05-18 16:34:09 +0700

    Jobsheet07_Praktikum02

--- Jobsheet05/app/Filament/Resources/Products/Schemas/ProductInfolist.php
+++ Jobsheet05/app/Filament/Resources/Products/Schemas/ProductInfolist.php
@@ -4,13 +4,75 @@ namespace App\Filament\Resources\Products\Schemas;
 
 use Filament\Schemas\Schema;
 
+use Filament\Schemas\Components\Section;
+use Filament\Infolists\Components\TextEntry;
+use Filament\Infolists\Components\ImageEntry;
+use Filament\Infolists\Components\IconEntry;
+
 class ProductInfolist
 {
     public static function configure(Schema $schema): Schema
     {
         return $schema
             ->components([
-                //
+                Section::make('Product Info')
+                    ->schema([
+
+                        TextEntry::make('name')
+                            ->label('Product Name')
+                            ->weight('bold')
+                            ->color('primary'),
+
+                        TextEntry::make('id')
+                            ->label('Product ID'),
+
+                        TextEntry::make('sku')
+                            ->label('Product SKU')
+                            ->badge()
+                            ->color('warning'),
+
+                        TextEntry::make('description')
+                            ->label('Product Description'),
+
+                    ])
+                    ->columnSpanFull(),
+
+                Section::make('Pricing & Stock')
+                    ->schema([
+
+                        TextEntry::make('price')
+                            ->label('Product Price')
+                            ->formatStateUsing(
+                            fn ($state) => 'Rp ' . number_format($state, 0, ',', '.'))
+                            ->icon('heroicon-o-currency-dollar'),
+
+                        TextEntry::make('stock')
+                            ->icon('heroicon-o-cube')
+                            ->label('Product Stock'),
+
+                    ]),
+
+                Section::make('Media & Status')
+                    ->schema([
+
+                        ImageEntry::make('image')
+                            ->label('Product Image')
+                            ->disk('public'),
+                        
+                        IconEntry::make('is_active')
+                            ->label('Is Active')
+                            ->boolean(),
+
+                        IconEntry::make('is_featured')
+                            ->label('Is Featured')
+                            ->boolean(),
+
+                        TextEntry::make('created_at')
+                            ->label('Product Creation Date')
+                            ->date('d M Y')
+                            ->color('info'),
+
+                    ]),
             ]);
     }
 }
